e7041c97331f6be39d9fe361e049915483be4f0f
monasrour <mona.omar.srour@gmail.com>
2023-09-19 16:16:43 +0300

    Enhance routing

--- routes/web.php
+++ routes/web.php
@@ -19,24 +19,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//Route::get('dashboard',function (){
-//    return view('dashboard');
-//});
-Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
 
-Route::get('dashboard/campanies',[CompanyController::class,'index'])->name('dashboard.campanies.index');
-Route::get('dashboard/campanies/create',[CompanyController::class,'create'])->name('dashboard.campanies.create');
-Route::post('dashboard/campanies/store',[CompanyController::class,'store'])->name('dashboard.campanies.store');
-Route::put('dashboard/campanies/update/{id}',[CompanyController::class,'update'])->name('dashboard.campanies.update');
-Route::get('dashboard/campanies/edit/{id}',[CompanyController::class,'edit'])->name('dashboard.campanies.edit');
-Route::delete('dashboard/campanies/store/{id}',[CompanyController::class,'destroy'])->name('dashboard.campanies.destroy');
-//----------------------------------
-Route::get('dashboard/employees',[EmployeeController::class,'index'])->name('dashboard.employees.index');
-Route::get('dashboard/employees/create',[EmployeeController::class,'create'])->name('dashboard.employees.create');
-Route::post('dashboard/employees/store',[EmployeeController::class,'store'])->name('dashboard.employees.store');
-Route::put('dashboard/employees/update/{id}',[EmployeeController::class,'update'])->name('dashboard.employees.update');
-Route::get('dashboard/employees/edit/{id}',[EmployeeController::class,'edit'])->name('dashboard.employees.edit');
-Route::delete('dashboard/employees/store/{id}',[EmployeeController::class,'destroy'])->name('dashboard.employees.destroy');
+
+
+Route::prefix('dashboard')->name('dashboard')->group(function (){
+    Route::get('/',[DashboardController::class,'index']);
+    Route::controller(CompanyController::class)->group(function (){
+        Route::prefix('campanies')->name('.campanies.')->group(function (){
+            Route::get('/','index')->name('index');
+            Route::get('/create','create')->name('create');
+            Route::post('/store','store')->name('store');
+            Route::put('/update/{id}','update')->name('update');
+            Route::get('/edit/{id}','edit')->name('edit');
+            Route::delete('/store/{id}','destroy')->name('destroy');
+        });
+        });
+
+    Route::controller(EmployeeController::class)->group(function (){
+        Route::prefix('employees')->name('.employees.')->group(function (){
+            Route::get('/','index')->name('index');
+            Route::get('/create','create')->name('create');
+            Route::post('/store','store')->name('store');
+            Route::put('/update/{id}','update')->name('update');
+            Route::get('/edit/{id}','edit')->name('edit');
+            Route::delete('/store/{id}','destroy')->name('destroy');
+        });
+        });
+
+});
 
 
 Auth::routes();
